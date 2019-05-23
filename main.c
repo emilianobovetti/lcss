c0b92786bba5bffbdf15a0d450da365995786a63
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-05-23 15:26:11 +0200

    some tests

--- main.c
+++ main.c
@@ -32,6 +32,20 @@ void print_lcss(tree_t *tree, node_t **lcss)
     }
 }
 
+bool check_end_sym(char end_sym)
+{
+    bool check = is_end_sym(end_sym);
+
+    if (!check)
+    {
+        fprintf(stderr, "Too many strings\n");
+        fprintf(stderr, "I can't handle more than %d strings :(\n", UCHAR_MAX - SCHAR_MAX);
+        fprintf(stderr, "(char '%d' isn't a valid end symbol)\n", end_sym);
+    }
+
+    return check;
+}
+
 joined_str_t *join_str_arr(char **strings)
 {
     size_t total_len = 1; // +1 for '\0'
@@ -49,14 +63,6 @@ joined_str_t *join_str_arr(char **strings)
 
     for (n_str = 0; strings[n_str] != NULL; n_str++)
     {
-        if (!is_end_sym(end_sym))
-        {
-            fprintf(stderr, "Too many strings\n");
-            fprintf(stderr, "I can't handle more than %d strings :(\n", UCHAR_MAX - SCHAR_MAX);
-            fprintf(stderr, "(char '%d' isn't a valid end symbol)\n", end_sym);
-            return NULL;
-        }
-
         char *cur_str = strings[n_str];
         size_t j = 0;
 
@@ -68,9 +74,12 @@ joined_str_t *join_str_arr(char **strings)
             j++;
         }
 
-        cat[cat_idx] = end_sym;
-        cat_idx++;
-        end_sym--;
+        cat[cat_idx++] = end_sym--; // TODO (?)
+
+        if (!check_end_sym(end_sym))
+        {
+            return NULL;
+        }
     }
 
     // don't forget to bring a towel
@@ -99,7 +108,75 @@ void test_with_static_strings(void)
     print_lcss(tree, lcss);
 }
 
+#define SIZE_CHUNK 1024
+
+joined_str_t *read_strings(FILE* fp)
+{
+    size_t cur_size = SIZE_CHUNK;
+    size_t len = 0;
+    char end_sym = UCHAR_MAX;
+    char cur_c;
+
+    char *str = calloc(cur_size, sizeof(char));
+
+    // TODO: free res when returing NULL
+    joined_str_t *res = malloc(sizeof(joined_str_t));
+    res->num_strings = 0;
+
+    if (str == NULL)
+    {
+        return NULL;
+    }
+
+    while (true)
+    {
+        cur_c = fgetc(fp);
+
+        if (cur_c == EOF)
+        {
+            break;
+        }
+
+        if (cur_c == '\n')
+        {
+            str[len++] = end_sym--;
+            res->num_strings++;
+
+            if (!check_end_sym(end_sym))
+            {
+                free(str);
+                return NULL;
+            }
+        }
+        else
+        {
+            str[len++] = cur_c;
+        }
+
+        if (len == SIZE_CHUNK)
+        {
+            cur_size += SIZE_CHUNK;
+            str = realloc(str, sizeof(char) * cur_size);
+
+            if (str == NULL)
+            {
+                return NULL;
+            }
+        }
+    }
+
+    str[len++] = '\0';
+
+    res->ptr = realloc(str, sizeof(char) * len);
+
+    return res;
+}
+
 void main(void)
 {
-    test_with_static_strings();
+    joined_str_t *join = read_strings(stdin);
+
+    tree_t *tree = build_tree(join->ptr, join->num_strings);
+    node_t **lcss = tree_to_lcss(tree);
+    print_lcss(tree, lcss);
 }
